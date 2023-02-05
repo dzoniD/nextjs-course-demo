9633c3f6ccfacc38364146a4d420caa8635dfcc2
dzoniD <edvard_dantes@hotmail.com>
2023-02-05 04:14:05 +0100

    ready for deployment

---
+++ pages/api/new-meetup.js
@@ -0,0 +1,27 @@
+import { MongoClient } from 'mongodb';
+
+// /api/new-meetup
+// POST /api/new-meetup
+
+async function handler(req, res) {
+  if (req.method === 'POST') {
+    const data = req.body;
+
+    const client = await MongoClient.connect(
+      'mongodb+srv://pera_peric:C5H3Ph9t9N1GdlDH@cluster0.egvayjy.mongodb.net/meetups?retryWrites=true&w=majority'
+    );
+    const db = client.db();
+
+    const meetupsCollection = db.collection('meetups');
+
+    const result = await meetupsCollection.insertOne(data);
+
+    console.log(result);
+
+    client.close();
+
+    res.status(201).json({ message: 'Meetup inserted!' });
+  }
+}
+
+export default handler;
